7f2ddf1fc4477d3e653da315f01db294a70dcd60
nagarajpandith <nagaraj.pandith2002@gmail.com>
2022-03-02 17:37:45 +0530

    new updates

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import NavBar from "../src/components/Navbar/index"
+import Hero from "../src/components/Hero/index"
 
 export default function Home() {
-  return (
-      <div>
+  return <>
       <Head>
-        <title>Portfolio</title>
-        <meta name="description" content="Portfolio site" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Nagaraj Pandith - Web Developer</title>
+        <link rel="icon" href="./logo-dark.png" />
+        <meta name="description" content="Nagaraj Pandith - Portfolio" />
+        <meta name="keywords" content="nagaraj, pandith, portfolio, web developer, ui designer, frontend web developer, django, nextjs, tailwind, react developer" />
+        <meta property="og:site_name" content="nagarajpandith.live" />
+        <meta property="og:title" content="Nagaraj Pandith" />
+        <meta property="og:description" content="Nagaraj Pandith - Web Developer Portfolio" />
       </Head>
-      <div className='text-center text-5xl mt-80'>Portfolio</div>
-      </div>
-  )
+
+      <NavBar />
+      <Hero />
+
+    </>
 }
---
+++ src/components/Hero/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div>Hello There</div>
+    )
+}
+
+export default Hero
