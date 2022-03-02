bb5f6fde0eb69ce63eef31f0a2a5cbf71327121a
nagarajpandith <nagaraj.pandith2002@gmail.com>
2022-03-02 23:41:49 +0530

    nextjs error fix

--- components/Experience.js
+++ components/Experience.js
@@ -10,18 +10,18 @@ export default function Experience() {
             </h2>
             </div>
             
-            <ol class="relative border-l border-gray-200 dark:border-gray-700">
-            <li class="mb-10 ml-4">
-            <div class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Oct 2021 - Present</time>
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Inspirante Technologies Pvt. Ltd.</h3>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Web Development Intern</p>
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+            <li className="mb-10 ml-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Oct 2021 - Present</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Inspirante Technologies Pvt. Ltd.</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Web Development Intern</p>
             </li>
-            <li class="mb-10 ml-4">
-            <div class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Jan 2022 - Present</time>
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Farmoid Robotech Pvt Ltd</h3>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Frontend Web Developer</p>
+            <li className="mb-10 ml-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Jan 2022 - Present</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Farmoid Robotech Pvt Ltd</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Frontend Web Developer</p>
             </li>
             </ol>
 
@@ -31,24 +31,24 @@ export default function Experience() {
             </h2>
             </div>
             
-            <ol class="relative border-l border-gray-200 dark:border-gray-700">
-            <li class="mb-10 ml-4">
-            <div class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2021 - Present</time>
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Bachelor of Engineering @ N.M.A.M.I.T.</h3>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Most recent CGPA: 8.90/10.00</p>
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+            <li className="mb-10 ml-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2021 - Present</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Bachelor of Engineering @ N.M.A.M.I.T.</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Most recent CGPA: 8.90/10.00</p>
             </li>
-            <li class="mb-10 ml-4">
-            <div class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2017 - 2019</time>
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Pre-University Education @ Poorna Prajna PUC</h3>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Aggregate percentage: 94% with A+ grade</p>
+            <li className="mb-10 ml-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2017 - 2019</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Pre-University Education @ Poorna Prajna PUC</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Aggregate percentage: 94% with A+ grade</p>
             </li>
-            <li class="mb-10 ml-4">
-            <div class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2014 - 2017</time>
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">High School Education @ I.E.M.H.S.</h3>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Aggregate percentage: 98.56%</p>
+            <li className="mb-10 ml-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2014 - 2017</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">High School Education @ I.E.M.H.S.</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Aggregate percentage: 98.56%</p>
             </li>
             </ol>
 
