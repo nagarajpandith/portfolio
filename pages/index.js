393a067a4affbf58aefa68036ef089e142bcc34b
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-03-19 23:44:13 +0530

    chore: outdate warning

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,22 @@ import Stack from '@/components/Stack';
 import About from '@/components/About';
 import Experience from '@/components/Experience';
 import Navbar from '@/components/Navbar';
+import Warning from '@/components/Warning';
+import { useState } from 'react';
 
 export default function Index() {
+  const [showPage, setShowPage] = useState(false);
+
+  const handleContinue = () => {
+    setShowPage(true);
+  };
+
   return (
     <>
-      <Navbar />
+    {!showPage && <Warning onContinue={handleContinue} />}
+      {showPage&&(
+        <>
+        <Navbar />
       <div className="p-6 lg:p-20 max-w-4xl m-auto w-full">
         <Hero />
         <div className="p-0 space-y-10">
@@ -18,6 +29,8 @@ export default function Index() {
           <Contact />
         </div>
       </div>
+        </>
+      )}
     </>
   );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -3,11 +3,21 @@ import Projects from '@/components/Projects';
 import Blogs from '@/components/Blogs';
 import Videos from '@/components/Videos';
 import Navbar from '@/components/Navbar';
+import Warning from '@/components/Warning';
+import { useState } from 'react';
 
 export default function Works() {
+  const [showPage, setShowPage] = useState(false);
+
+  const handleContinue = () => {
+    setShowPage(true);
+  };
   return (
     <>
-      <Navbar />
+    {!showPage && <Warning onContinue={handleContinue} />}
+      {showPage&&(
+        <>
+        <Navbar />
       <div className="p-6 lg:p-20 max-w-4xl m-auto w-full">
         <div className="p-0 space-y-10">
           <Projects />
@@ -16,6 +26,8 @@ export default function Works() {
           <Contact />
         </div>
       </div>
+        </>
+      )}
     </>
   );
 }
